d1f4caa8873dfeccb70f05af69dae5616834b85d
Stephen Sherratt <stephen@sherra.tt>
2023-03-17 16:06:59 +1100

    Remove custom overlay repo

--- generate/lib/helpers.ml
+++ generate/lib/helpers.ml
@@ -3,12 +3,7 @@ open Switch_builder
 let cached_repo_with_overlay () =
   let opam_repo = Repository.path "./data/repos/opam-repository" in
   let opam_overlays = Repository.path "./data/repos/opam-overlays" in
-  let custom_opam_overlays = Repository.path "./custom-overlays" in
-  let combined_repo =
-    Repository.combine
-      (Repository.combine opam_repo opam_overlays)
-      custom_opam_overlays
-  in
+  let combined_repo = Repository.combine opam_repo opam_overlays in
   let cache = Repository.Cache.create () in
   let cached_repo = Repository.cache cache combined_repo in
   cached_repo
