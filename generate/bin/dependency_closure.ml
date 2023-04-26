d80679c0911a07a289f2cdf0ce8f84b6cd1473b2
Stephen Sherratt <stephen@sherra.tt>
2023-04-26 14:49:22 +1000

    Change cli to monorepo generator

--- generate/bin/dependency_closure.ml
+++ generate/bin/dependency_closure.ml
@@ -2,16 +2,31 @@
    closed under dependency, co-installable, and where each package can be built
    with dune. *)
 
+module Args = struct
+  type t = { arch : string; allow_depexts : bool }
+
+  let parse () =
+    let arch = ref "x86_64" in
+    let allow_depexts = ref false in
+    let specs =
+      [
+        ("--arch", Arg.Set_string arch, "architecture to target");
+        ( "--allow-depexts",
+          Arg.Set allow_depexts,
+          "include packages with external dependencies" );
+      ]
+    in
+    Arg.parse specs
+      (fun _ -> failwith "unexpected anonymous argument")
+      "print out a list of packages to include in monorepo";
+    { arch = !arch; allow_depexts = !allow_depexts }
+end
+
 let () =
   Logs.set_reporter (Logs_fmt.reporter ());
   Logs.set_level (Some Logs.Info);
-  let arch =
-    if Array.length Sys.argv < 2 then failwith "missing arch"
-    else Array.get Sys.argv 1
-  in
+  let { Args.arch; allow_depexts } = Args.parse () in
   let repo = Helpers.Policy.cached_repo_with_overlay () in
-  let set =
-    Helpers.Policy.large_closed_package_set ~arch ~allow_depexts:true repo
-  in
+  let set = Helpers.Policy.large_closed_package_set ~arch ~allow_depexts repo in
   print_endline
     (OpamPackage.Set.elements set |> Helpers.Packages.to_string_pretty)
