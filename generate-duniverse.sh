bf9c7cce0ed0ab674b4eed2ebdd938fb379fab9f
Stephen Sherratt <stephen@sherra.tt>
2023-07-11 11:29:00 +1000

    Show log while generating duniverse

--- generate-duniverse.sh
+++ generate-duniverse.sh
@@ -4,12 +4,12 @@
 # Run `./generate-duniverse.sh /path/to/output` and it will create the directory
 # /path/to/output/duniverse
 
-set -eu
+set -eux
 
 OUTPUT_DIR=$1
 
 pushd benchmark > /dev/null
-IMAGE_ID=$(docker build . -q)
+docker build . --tag=generate-duniverse
 popd > /dev/null
 
-docker run --rm -it --volume=$OUTPUT_DIR:/output_dir $IMAGE_ID sudo cp -a duniverse /output_dir
+docker run --rm -it --volume=$OUTPUT_DIR:/output_dir generate-duniverse sudo cp -a duniverse /output_dir
